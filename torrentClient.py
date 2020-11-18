eec68c7bd82c69866427b17819fc54f016fd4eae
Lee <richmond.liew.97@gmail.com>
2020-11-18 15:30:01 -0500

    added a Tracker

--- torrentClient.py
+++ torrentClient.py
@@ -1,8 +1,39 @@
 import bencodepy
+import os
+import requests
 
 class Client:
     def __init__(self, torrentFile):
         self.torrentFile = torrentFile
+        self.peer_id = self.getNewPeerId()
+
+    def getNewPeerId(self):
+        return os.urandom(20)
+
+    def startDownload(self):
+        self.peer_id = self.getNewPeerId()
+        announceUrl = self.torrentFile.getAnnounceUrl()
+        tracker = Tracker(announceUrl, self.peer_id, 6881)
+        res = tracker.get(self.torrentFile.getPiece(0))
+        return res.text
+
+class Tracker:
+    def __init__(self,announce_url, peer_id, port_number):
+        self.announce_url = announce_url
+        self.peer_id = peer_id
+        self.listen_port_number = port_number
+
+    def get(self, info_hash):
+        reqData = {
+            'info_hash': info_hash,
+            'peer_id': self.peer_id,
+            'port': self.listen_port_number,
+            'uploaded': 0,
+            'downloaded':0
+        }
+        res = requests.get(self.announce_url, reqData)
+        return res
+
 
 class TorrentFile:
     def __init__(self, filePath):
@@ -29,5 +60,6 @@ class TorrentFile:
 
 if __name__ == "__main__":
     torrentFile = TorrentFile('./fanimatrix.torrent')
-    print(torrentFile.getPiece(514))
-    print(torrentFile.getPiece(515))
+    client = Client(torrentFile)
+    res = client.startDownload()
+    print(res)
