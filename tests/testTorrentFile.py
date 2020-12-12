e0f49a797f63a86c47962c98c87c65abb4fdabe3
Lee <richmond.liew.97@gmail.com>
2020-12-11 20:45:20 -0500

    added torrentFile tests

---
+++ tests/testTorrentFile.py
@@ -0,0 +1,18 @@
+import unittest
+from pyTorrent.torrentFile import TorrentFile
+
+class TorrentFileTest(unittest.TestCase):
+    def testDecodeTorrentFile(self):
+        torrentFile = TorrentFile('../resources/fanimatrix.torrent')
+        self.assertEqual(torrentFile.piece_length, 262144)
+
+    def testIsLastPiece(self):
+        torrentFile = TorrentFile('../resources/fanimatrix.torrent')
+        self.assertEqual(torrentFile.isFinalPiece(515), True)
+        self.assertEqual(torrentFile.getFinalPieceLength(), 42414)
+        self.assertEqual(torrentFile.getNBlocksFinalPiece(), 3)
+        self.assertEqual(torrentFile.getNBlocks(50), 16)
+        self.assertEqual(torrentFile.getNBlocks(515), 3)
+        self.assertEqual(torrentFile.getBlockSize(50, 10), pow(2,14))
+        self.assertEqual(torrentFile.getBlockSize(515, 2), 9646)
+
